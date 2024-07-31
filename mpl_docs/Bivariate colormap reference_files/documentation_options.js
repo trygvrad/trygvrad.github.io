e4ea1d981b7c9f35ad236b8eb58bdf89e7baeec6
Trygve Magnus Ræder <trygvrad@gmail.com>
2024-07-31 15:41:32 +0200

    more docs

---
+++ mpl_docs/Bivariate colormap reference_files/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '3.10.0.dev375+g81181c94af.d20240709',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
