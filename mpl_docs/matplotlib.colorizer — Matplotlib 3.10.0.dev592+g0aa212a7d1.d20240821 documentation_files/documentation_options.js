eafbcc9967742a408f28ac973ee8c4671fa3cf99
Trygve Magnus Ræder <trygvrad@gmail.com>
2024-08-22 10:02:07 +0200

    mpl colorizer docs

---
+++ mpl_docs/matplotlib.colorizer — Matplotlib 3.10.0.dev592+g0aa212a7d1.d20240821 documentation_files/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '3.10.0.dev592+g0aa212a7d1.d20240821',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
